9ccc820734bc8ef9fb2815984d8bd3e5d87d69dc
Frithjof Pfannstiel <frithjof.pfannstiel@gfe-media.de>
2020-06-24 11:44:44 +0200

    basic rules to make recap v3 work

---
+++ app/code/Gfe/Csp/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Frithjof Pfannstiel
+ * Github: https://github.com/frithjof
+ * Date: 24.06.20
+ * Time: 11:27
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE, 'Gfe_Csp',
+    __DIR__
+);
